680297442c9038eb1974b79874213b0d7a8c2501
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-02-24 11:25:41 -0500

    Sort an array of double using the bubble sort algorithm

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -12,21 +12,42 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		
-		float[] sortedNums = getNumbersFromCase();
+		double[] sortedNums = getNumbersFromCase();
 		System.out.println(Arrays.toString(sortedNums));
+		bubbleSort(sortedNums);
+		
+		//double a = 0.8;
+		
 	}
 	
 	
-	//Read one line of the console and returns an array of float numbers from the input separated by an space
-	public static float[] getNumbersFromCase() throws IOException {
+	//Read one line of the console and returns an array of double numbers from the input separated by an space
+	public static double[] getNumbersFromCase() throws IOException {
 		String[] caseX = br.readLine().split(" ");
-		float[] caseXNumbers = new float[caseX.length];
+		double[] caseXNumbers = new double[caseX.length];
 		
-		for (float i = 0; i < caseXNumbers.length; i++) {
-			caseXNumbers[(int) i] = Float.parseFloat(caseX[(int) i]);
+		for (double i = 0; i < caseXNumbers.length; i++) {
+			caseXNumbers[(int)i] = Double.parseDouble(caseX[(int) i]);
 		}
 		
 		return caseXNumbers;
 	}//end getNumbersFromCase
-
+	
+	//Sort an array of double numbers
+	public static double[] bubbleSort(double[] array) {
+		
+		boolean changed = true;
+		for (int i = 1; i < array.length && changed; i++) {
+			changed = false;
+			for (int j = 0; j < array.length-i; j++) {
+				if(array[j]>array[j+1]) {
+					double temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					changed = true;
+				}	
+			}
+		}
+		return array;
+	}//end bubbleSort
 }
