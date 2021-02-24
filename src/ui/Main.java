f83b5f38ac36033fe9a310a4fb9e78333009e94d
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-02-24 12:08:35 -0500

    Added method that prints the avearage of changes per iteration during the sorting proces and the array sorted in the specified format

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,20 +6,22 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
+	//Atributes
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static double iterations = 0; 
+	public static double changesPerIteration = 0;
 	
 	public static void main(String[] args) throws IOException{
 		
 		double[] sortedNums = getNumbersFromCase();
+		
 		System.out.println(Arrays.toString(sortedNums));
-		bubbleSort(sortedNums);
 		
-		//double a = 0.8;
+		sortedNums = bubbleSort(sortedNums);
 		
+		printOutput(sortedNums);
 	}
-	
 	
 	//Read one line of the console and returns an array of double numbers from the input separated by an space
 	public static double[] getNumbersFromCase() throws IOException {
@@ -33,9 +35,7 @@ public class Main {
 		return caseXNumbers;
 	}//end getNumbersFromCase
 	
-	//Sort an array of double numbers
 	public static double[] bubbleSort(double[] array) {
-		
 		boolean changed = true;
 		for (int i = 1; i < array.length && changed; i++) {
 			changed = false;
@@ -45,9 +45,27 @@ public class Main {
 					array[j] = array[j+1];
 					array[j+1] = temp;
 					changed = true;
-				}	
+					changesPerIteration++;
+				}
 			}
+		iterations++;
 		}
 		return array;
 	}//end bubbleSort
+	
+	public static void printOutput(double[] sortedNums) {
+		double avearage = Math.round((changesPerIteration/iterations) * 100) / 100d;
+		System.out.print(avearage+"-");
+		for (int i = 0; i < sortedNums.length; i++) {
+			if(i!=sortedNums.length-1) {
+				System.out.print(sortedNums[i]+" ");
+			}else {
+				System.out.print(sortedNums[i]);
+			}
+			
+		}
+		iterations = 0;
+		changesPerIteration = 0;
+	}// end printOutput
+	
 }
