233ae91bd1ab22e88d58ca9d2052738b2ccc9711
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-02-24 10:51:49 -0500

    Added a method that translates a single input line into an array of float numbers from the values separated by an space

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,32 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	
+	public static void main(String[] args) throws IOException{
+		
+		float[] sortedNums = getNumbersFromCase();
+		System.out.println(Arrays.toString(sortedNums));
 	}
+	
+	
+	//Read one line of the console and returns an array of float numbers from the input separated by an space
+	public static float[] getNumbersFromCase() throws IOException {
+		String[] caseX = br.readLine().split(" ");
+		float[] caseXNumbers = new float[caseX.length];
+		
+		for (float i = 0; i < caseXNumbers.length; i++) {
+			caseXNumbers[(int) i] = Float.parseFloat(caseX[(int) i]);
+		}
+		
+		return caseXNumbers;
+	}//end getNumbersFromCase
 
 }
