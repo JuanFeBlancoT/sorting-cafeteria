2557f7ab78df7db813b470aabb76339d3e9a60af
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-02-25 10:21:26 -0500

    Corrected avearage calculation and the program now reads n numbers of lines

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,7 +3,8 @@ package ui;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class Main {
 	
@@ -11,21 +12,33 @@ public class Main {
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static double iterations = 0; 
 	public static double changesPerIteration = 0;
-	
+	public static DecimalFormat df = new DecimalFormat("###.##");
+
+
 	public static void main(String[] args) throws IOException{
 		
-		double[] sortedNums = getNumbersFromCase();
+		int numberOfCases = Integer.parseInt(br.readLine());
+		System.out.println("\n");
+		int i = 0;
 		
-		System.out.println(Arrays.toString(sortedNums));
-		
-		sortedNums = bubbleSort(sortedNums);
-		
-		printOutput(sortedNums);
+		while(i<numberOfCases) {
+						
+			double[] sortedNums = getNumbersFromCase();
+			sortedNums = bubbleSort(sortedNums);
+			printOutput(sortedNums);
+			
+			iterations = 0;
+			changesPerIteration = 0;
+
+			i++;
+		}
+		br.close();
 	}
 	
 	//Read one line of the console and returns an array of double numbers from the input separated by an space
 	public static double[] getNumbersFromCase() throws IOException {
 		String[] caseX = br.readLine().split(" ");
+		
 		double[] caseXNumbers = new double[caseX.length];
 		
 		for (double i = 0; i < caseXNumbers.length; i++) {
@@ -36,36 +49,34 @@ public class Main {
 	}//end getNumbersFromCase
 	
 	public static double[] bubbleSort(double[] array) {
-		boolean changed = true;
-		for (int i = 1; i < array.length && changed; i++) {
-			changed = false;
+		
+		for (int i = 1; i < array.length ; i++) {
 			for (int j = 0; j < array.length-i; j++) {
 				if(array[j]>array[j+1]) {
 					double temp = array[j];
 					array[j] = array[j+1];
 					array[j+1] = temp;
-					changed = true;
 					changesPerIteration++;
 				}
+				
 			}
-		iterations++;
+			iterations++;
 		}
 		return array;
 	}//end bubbleSort
 	
 	public static void printOutput(double[] sortedNums) {
-		double avearage = Math.round((changesPerIteration/iterations) * 100) / 100d;
-		System.out.print(avearage+"-");
+		double avearage = changesPerIteration/iterations;
+		df.setRoundingMode(RoundingMode.DOWN);
+		System.out.print(df.format(avearage).replace(",", ".")+"-");
 		for (int i = 0; i < sortedNums.length; i++) {
 			if(i!=sortedNums.length-1) {
 				System.out.print(sortedNums[i]+" ");
 			}else {
-				System.out.print(sortedNums[i]);
+				System.out.println(sortedNums[i]);
 			}
 			
 		}
-		iterations = 0;
-		changesPerIteration = 0;
 	}// end printOutput
 	
 }
